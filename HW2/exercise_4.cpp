7f7fec664d143e9d2b1bc8a74a8c4e5aee080e60
Julius Gruber <gruber.julius@gmx.at>
2023-10-01 15:15:45 -0400

    add final version for hw2 4

--- HW2/exercise_4.cpp
+++ HW2/exercise_4.cpp
@@ -11,6 +11,33 @@ double max(double a, double b){
 	return a;
 }
 
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+// Linear Congruential Generator parameters
+const unsigned long long a = 39373;
+const unsigned long long c = 0;
+const unsigned long long k = 2147483647; // 2^31 - 1
+
+double generateUniform() {
+    static unsigned long long xi = 1;
+    xi = (a * xi + c) % k;
+    return static_cast<double>(xi) / k;
+}
+
+void boxMuller(double& z1, double& z2) {
+    double u1 = generateUniform();
+    double u2 = generateUniform();
+
+    double r = sqrt(-2.0 * log(u1));
+    double theta = 2.0 * M_PI * u2;
+
+    z1 = r * cos(theta);
+    z2 = r * sin(theta);
+}
+
 
 int main(){
     double lambda = 4;
@@ -46,18 +73,21 @@ int main(){
 	    S[0] = 50;
    	    V[0] = 0.09;
         for(int j=0; j < n; j++){
-
             double T = 0.5;
             double dt = 0.5/175.;
             for(int i = 1; i < 87; i++){
-                double z1 = distribution(gen);
-                double z2 = distribution(gen);
+
+                double z1, z2;
+                boxMuller(z1, z2);
+                //double z1 = distribution(gen);
+                //double z2 = distribution(gen);
 		        S[i] = S[i-1] * std::exp( (r - max(V[i-1], 0)/2)*dt + std::sqrt(max(V[i-1], 0) )* std::sqrt(dt)*z1);
-//                std::cout << "S " << S[i] << std::endl;
-                V[i] = max(V[i-1], 0) - lambda *( max(V[i-1], 0) -Vbar) * dt + eta * max(V[i-1], 0) * std::sqrt(dt) * ( rho* z1  + std::sqrt(1  - rho*rho) * z2);
+                //std::cout << "S " << S[i] << std::endl;
+                V[i] = max(V[i-1], 0) - lambda *( max(V[i-1], 0) -Vbar) * dt + eta * std::sqrt(max(V[i-1], 0)) * std::sqrt(dt) * ( rho* z1  + std::sqrt(1  - rho*rho) * z2);
                 //std::cout << "V " << V[i] << std::endl;
                 //std::cout << "blackscholes " << blackScholesCall(S[i], K,  T, std::sqrt(V[i]), 0.0,  r)  << std::endl;
-                stor.push_back(blackScholesCall(S[i], K,  T, std::sqrt(V[i]), 0.0,  r));
+                //stor.push_back(blackScholesCall(S[i], K,  T, std::sqrt(V[i]), 0.0,  r));
+                stor.push_back(blackScholesPut(S[i], K,  T, std::sqrt(V[i]), 0.0,  r));
                 vola.push_back(std::sqrt(V[i]));
                 under.push_back(S[i]);
                 T -= dt;
