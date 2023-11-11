5fbb8ee4873859a77eb8af10175a77b4d795a849
ycsun666 <phoenix.ycsun@gmail.com>
2023-11-11 15:41:04 -0500

    initial_commit

---
+++ MTH9815/LinkedList/LinkedList/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LinkedList
+//
+//  Created by Yicheng.Sun on 11/11/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
